17720bbf8a8f4e7e4d9f36664c3e6689bedb1ef9
Bi2Nb9O3 <me@bi2nb9o3.xyz>
2024-01-31 11:27:36 +0800

    SC:Step 1 for Cutsom Scren

--- js/canvasControl.js
+++ js/canvasControl.js
@@ -1,6 +1,49 @@
 import * as LOGGER from "/js/logging.js";
 import * as THREE from "/js/lib/three.module.js";
 
+/**
+ * @description 屏幕对象
+ * @param {int} uuid - UUID
+ * @param {THREE.Scene} scene - 父场景
+ * @param {float} width - 屏幕宽度
+ * @param {float} height - 屏幕高度
+ * @param {float} scale - 缩放大小(px=>米)
+ * @param {Array} pics - 图片数组
+ * @param {Array} objInfo - 物体信息
+ * @param {int} padding - 内边距
+ * @param {JSON}
+ */
+class Screen {
+    constructor(
+        uuid,
+        scene,
+        width,
+        height,
+        scale = 0.001,
+        pics,
+        objInfo, //[[x,y,z],[rx,ry,rz]]
+        padding,
+        imgInfo
+    ) {
+        this.uuid = uuid | ("scr" + uuid);
+        this.scene = scene;
+        this.width = width;
+        this.height = height;
+        this.scale = scale;
+        this.pics = pics;
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.padding = padding;
+        var context = canvas.getContext("2d");
+        context.fillStyle = "#c7e8ff";
+        context.fillRect(0, 0, canvassz[0], canvassz[1]);
+    }
+}
+
+/**
+ * @description 图像绘制类
+ */
 export class CanvasPainter {
     constructor(start, stop, infoURL, cavObjArray) {
         this.start = start;
@@ -14,6 +57,10 @@ export class CanvasPainter {
         this.isInited = 0;
         this.imgInfo;
     }
+
+    /**
+     * @description 加载信息
+     */
 
     loadInfo() {
         let that = this;
@@ -37,6 +84,10 @@ export class CanvasPainter {
                 console.log(error);
             });
     }
+
+    /**
+     * @description 加载图片
+     */
     loadImg() {
         for (var i = this.start; i <= this.stop; i++) {
             let loadingfile = new Image();
@@ -72,10 +123,11 @@ export class CanvasPainter {
             this.images = [...this.images, loadingfile];
         }
     }
+
     /**
      * @description 初始化canvas画图
      * @param {Array} cavObjArray - 包含canvas对象和位置信息的数组
-     * @param {Object} scene - Three.js场景对象
+     * @param {THREE.Scene} scene - Three.js场景对象
      */
     canvasInitPaint(cavObjArray, scene) {
         //Custom Settings
@@ -87,17 +139,10 @@ export class CanvasPainter {
             this.cavobj = cavObjArray;
         }
 
-        //=======================================
-        var cnt = 0;
+        //process
         that.cavobj.forEach((ele) => {
             var canvassz = ele[0];
-            var canvas = document.createElement("canvas");
-            canvas.width = canvassz[0];
-            canvas.height = canvassz[1];
             var objpos = ele[1];
-            var context = canvas.getContext("2d");
-            context.fillStyle = "#c7e8ff";
-            context.fillRect(0, 0, canvassz[0], canvassz[1]);
             var image = that.images[cnt];
             var imageinfo = that.imgInfo[cnt];
             context.drawImage(
@@ -116,8 +161,8 @@ export class CanvasPainter {
             );
             context.fillStyle = "black";
             context.font = "60px sans-serif";
-            console.log(context.font)
-            context.fillText(imageinfo['学生姓名'], 50, canvas.height-20);
+            console.log(context.font);
+            context.fillText(imageinfo["学生姓名"], 50, canvas.height - 20);
             var texture = new THREE.CanvasTexture(canvas);
             var material = new THREE.MeshBasicMaterial({
                 map: texture,
@@ -132,7 +177,11 @@ export class CanvasPainter {
             );
             obj.position.set(objpos[0], objpos[1], objpos[2]);
             obj.name = "screen" + (cnt + 1);
-            obj.rotation.y = 1.570796;//弧度数据
+
+            obj.rotation.x = ele[2][0] * (180 / Math.PI);
+            obj.rotation.y = ele[2][1] * (180 / Math.PI);
+            obj.rotation.z = ele[2][2] * (180 / Math.PI);
+
             scene.add(obj);
             console.log(canvas);
             obj.material.needsUpdate = true;
--- js/nsba.js
+++ js/nsba.js
@@ -2,9 +2,12 @@ import WebGL from "./lib/capabilitiesChecker.js";
 
 function getBrowserVersion() {
     var userAgent = window.navigator.userAgent;
-
+    if(!userAgent){
+        return
+    }
     // IE
     if (/MSIE|Trident/.test(userAgent)||userAgent.indexOf("Opera Mini")!=-1) {
+        alert("Is Opera Mini||IE.Skipped")
         window.location.href = "error_browser.html"; //遇到IE 就放弃
     }
 
@@ -14,6 +17,7 @@ function getBrowserVersion() {
         var minimumChromeVersion = 51; // 设置最低支持的版本
 
         if (chromeVersion < minimumChromeVersion) {
+            alert("Is Chrome. "+chromeVersion)
             window.location.href = "error_browser.html";
         }
     }
@@ -24,6 +28,7 @@ function getBrowserVersion() {
         var minimumFirefoxVersion = 54; // 设置最低支持的版本
 
         if (firefoxVersion < minimumFirefoxVersion) {
+            alert("Is Firefox. "+firefoxVersion)
             window.location.href = "error_browser.html";
         }
     }
@@ -33,6 +38,7 @@ function getBrowserVersion() {
         var safariVersion = parseInt(RegExp.$1);
         var minimumSafariVersion = 10; // 设置最低支持的版本
         if (safariVersion < minimumSafariVersion) {
+            alert("Is Safari. "+safariVersion)
             window.location.href = "error_browser.html";
         }
     }
@@ -43,6 +49,7 @@ function getBrowserVersion() {
         var minimumEdgeVersion = 79; // 设置最低支持的版本
 
         if (edgeVersion < minimumEdgeVersion) {
+            alert("Is Edge. "+edgeVersion)
             window.location.href = "error_browser.html";
         }
     }
@@ -53,14 +60,12 @@ function getBrowserVersion() {
         var minimumOperaVersion = 38; // 设置最低支持的版本
 
         if (operaVersion < minimumOperaVersion) {
+            alert("Is Opera. "+operaVersion)
             window.location.href = "error_browser.html";
         }
     }
 }
 
 export function isSupported() {
-    if(WebGL.isWebGLAvailable()){
-        window.location.href = "error_browser.html";
-    }
     getBrowserVersion();
 }
