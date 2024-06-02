ad96b10546d5613968f58b8c5563cb1fd1eda47e
Bi2Nb9O3 <92721357+Bi2Nb9O3-Studio@users.noreply.github.com>
2024-06-03 00:03:49 +0800

    style check

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-
 # 九鼎.js
 
 `大禹所铸九鼎，将天下珍禽异兽、物产贡赋铸于其上，象征九州，这便是最早的贡赋物产的图像展示`
 
 ## To-do
 
-[ ]  Apply more config
-[ ]  Adapt to configs
+[ ] Apply more config
+[ ] Adapt to configs
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
     {
@@ -11,30 +11,24 @@ export default [
                 ...globals.browser
             }
         },
-        ignores: [
-            '.eslintrc.js',
-            '.prettierrc.js',
-            'babel.config.js',
-            '**/node_modules/**',
-            '**/Deprecated/**'
-        ],
-        files: ['**/*.js'],
+        ignores: [".eslintrc.js", ".prettierrc.js", "babel.config.js", "**/node_modules/**", "**/Deprecated/**"],
+        files: ["**/*.js"],
         rules: {
             ...eslintPluginPrettier.configs.recommended.rules,
             ...eslintConfigPrettier.rules,
-            semi: [2, 'always'], // 语句强制分号结尾
-            quotes: [2, 'double'], // 引号类型 ""
-            'no-alert': 0, // 禁止使用alert
+            semi: [2, "always"], // 语句强制分号结尾
+            // quotes: [2, 'double'], // 引号类型 ""
+            "no-alert": 0, // 禁止使用alert
             // 'no-console': 2, // 禁止使用console
-            'no-const-assign': 2, // 禁止修改const声明的变量
-            'no-debugger': 2, // 禁止使用debugger
-            'no-duplicate-case': 2, // switch中的case标签不能重复
-            'no-extra-semi': 2, // 禁止多余的冒号
-            'no-multi-spaces': 1, // 不能用多余的空格
-            'prettier/prettier': 'error'
+            "no-const-assign": 2, // 禁止修改const声明的变量
+            "no-debugger": 2, // 禁止使用debugger
+            "no-duplicate-case": 2, // switch中的case标签不能重复
+            "no-extra-semi": 2, // 禁止多余的冒号
+            "no-multi-spaces": 1, // 不能用多余的空格
+            "prettier/prettier": "error"
         },
         plugins: {
             prettier: eslintPluginPrettier
         }
     }
-]
+];
